d753878a470f97a771e8afcf01816045d9a587ee
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-11-04 09:35:30 +0000

    Fix requirements file for deployment issues

--- src/data_management.py
+++ src/data_management.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-@st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_btc_data():
     df = pd.read_csv("outputs/datasets/collection/BTCDaily.csv")
     return df
